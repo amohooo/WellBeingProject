c8f6e2f57dcaa24fe6ef0cfe728d7fe2a88b48ff
amohooo <imhaom@gmail.com>
2024-05-23 20:12:30 +1000

    Add chatServer

---
+++ src/main/java/com/cab302/wellbeing/model/ChatServer.java
@@ -0,0 +1,106 @@
+package com.cab302.wellbeing.model;//Source:
+//  Creating a simple Chat Client/Server Solution 
+//  http://pirate.shu.edu/~wachsmut/Teaching/CSAS2214/Virtual/Lectures/chat-client-server.html
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer implements Runnable {
+    private ChatServerThread[] clients = new ChatServerThread[50];
+    private ServerSocket server = null;
+    private Thread thread = null;
+    private int clientCount = 0;
+    private volatile boolean running = true;
+
+    public ChatServer(int port) {
+        try {
+            System.out.println("Binding to port " + port + ", please wait  ...");
+            server = new ServerSocket(port);
+            System.out.println("Server started: " + server);
+        } catch (IOException ioe) {
+            System.out.println("Cannot bind to port " + port + ": " + ioe.getMessage());
+        }
+    }
+
+    public void run() {
+        while (running) {
+            try {
+                System.out.println("Waiting for a client ...");
+                addThread(server.accept());
+            } catch (IOException ioe) {
+                if (running) {
+                    System.out.println("Server accept error: " + ioe);
+                }
+                stop();
+            }
+        }
+    }
+
+    public void stop() {
+        running = false;
+        if (thread != null) {
+            try {
+                if (server != null && !server.isClosed()) {
+                    server.close();
+                }
+            } catch (IOException ioe) {
+                System.out.println("Error closing server socket: " + ioe.getMessage());
+            }
+            thread = null;
+        }
+    }
+
+    private synchronized int findClient(int ID) {
+        for (int i = 0; i < clientCount; i++) {
+            if (clients[i].getID() == ID) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public synchronized void handle(int ID, String input) {
+        if (input.equals(".bye")) {
+            clients[findClient(ID)].send(".bye");
+            remove(ID);
+        } else {
+            for (int i = 0; i < clientCount; i++) {
+                clients[i].send(ID + ": " + input);
+            }
+        }
+    }
+
+    public synchronized void remove(int ID) {
+        int pos = findClient(ID);
+        if (pos >= 0) {
+            ChatServerThread toTerminate = clients[pos];
+            System.out.println("Removing client thread " + ID + " at " + pos);
+            if (pos < clientCount - 1) {
+                for (int i = pos + 1; i < clientCount; i++) {
+                    clients[i - 1] = clients[i];
+                }
+            }
+            clientCount--;
+            toTerminate.connClose(); // No need for try-catch here since close() no longer throws IOException
+        }
+    }
+
+    private void addThread(Socket socket) {
+        if (clientCount < clients.length) {
+            System.out.println("Client accepted: " + socket);
+            clients[clientCount] = new ChatServerThread(this, socket);
+            try {
+                clients[clientCount].open();
+                clients[clientCount].start();
+                clientCount++;
+            } catch (IOException ioe) {
+                System.out.println("Error opening thread: " + ioe);
+            }
+        } else {
+            System.out.println("Client refused: maximum " + clients.length + " reached.");
+        }
+    }
+}
+
